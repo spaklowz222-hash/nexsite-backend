7b0810ee580adf779fe1f2dc95e7be919c5cac56
spaklowz222-hash <spaklowz222@gmail.com>
2025-08-19 23:50:08 +0200

    fix: database SSL config for Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // rend les variables .env accessibles partout
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      ssl: process.env.DB_SSL === 'true', // Render impose SSL
-      autoLoadEntities: true,
-      synchronize: true, // ⚠️ à mettre sur false plus tard en prod
+      entities: [User],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
