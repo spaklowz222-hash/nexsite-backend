76faccf24324ed3321900172288d99261f7c7857
spaklowz222-hash <spaklowz222@gmail.com>
2025-08-19 20:03:57 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // Render injectera cette variable
+      autoLoadEntities: true,        // Charge automatiquement les entités
+      synchronize: true,             // ⚠️ crée/modifie les tables automatiquement (mettre false en prod + utiliser migrations)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
